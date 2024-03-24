94ab383fed320008e8ce1164696e12cdf48ef27a
AnishRane <anish.rane21@outlook.com>
2024-03-24 16:08:17 +0530

    fix: husky and es-lint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "env": {
+     "browser": true,
+     "amd": true,
+     "node": true // Add this line to specify the Node.js environment
+  },
+  "parserOptions": {
+     "ecmaVersion": 6
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+     "no-console": "off",
+     "indent": [
+       "error",
+       "tab"
+     ],
+     "linebreak-style": [
+       "error",
+       "windows"
+     ],
+     "quotes": [
+       "error",
+       "single"
+     ],
+     "semi": [
+       "error",
+       "always"
+     ]
+  }
+ };
+ 
